0e411d8b2f79cb7abf4b924730b191fa3f5fc39f
Witold <wkowalski.ai@gmail.com>
2025-04-17 18:24:57 +0200

    Add recipe management functionality with CRUD operations and templates

--- app/recipe_routes.py
+++ app/recipe_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
 from .models import Recipe
+from . import db
+from .forms import RecipeForm
 
 recipe_bp = Blueprint('recipe', __name__)
 
@@ -10,3 +12,44 @@ recipe_bp = Blueprint('recipe', __name__)
 def recipe_list():
     recipes = Recipe.query.filter_by(user_id=current_user.id).order_by(Recipe.updated_at.desc()).all()
     return render_template('recipe/index.html', recipes=recipes)
+
+@recipe_bp.route('/recipes/new', methods=['GET', 'POST'])
+@login_required
+def create_recipe():
+    form = RecipeForm()
+    if form.validate_on_submit():
+        new_recipe = Recipe(
+            title=form.title.data,
+            content=form.content.data,
+            user_id=current_user.id
+        )
+        db.session.add(new_recipe)
+        db.session.commit()
+        flash('Przepis został dodany!', 'success')
+        return redirect(url_for('recipe.recipe_list'))
+    return render_template('recipe/create_recipe.html', form=form)
+
+@recipe_bp.route('/recipes/<int:recipe_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+    if recipe.user_id != current_user.id:
+        flash('Brak dostępu do tego przepisu.', 'danger')
+        return redirect(url_for('recipe.recipe_list'))
+    form = RecipeForm(obj=recipe)
+    if form.validate_on_submit():
+        recipe.title = form.title.data
+        recipe.content = form.content.data
+        db.session.commit()
+        flash('Przepis został zaktualizowany!', 'success')
+        return redirect(url_for('recipe.view_recipe', recipe_id=recipe.id))
+    return render_template('recipe/edit_recipe.html', form=form, recipe=recipe)
+
+@recipe_bp.route('/recipes/<int:recipe_id>')
+@login_required
+def view_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+    if recipe.user_id != current_user.id:
+        flash('Brak dostępu do tego przepisu.', 'danger')
+        return redirect(url_for('recipe.recipe_list'))
+    return render_template('recipe/view_recipe.html', recipe=recipe)
